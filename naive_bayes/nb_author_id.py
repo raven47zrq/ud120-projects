6f6bff9ef23892396ae002e768e9d7a7d31fea00
Rongqian Zhang <rqzhang47@outlook.com>
2018-04-22 22:39:59 +0800

    nb & svm

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
+"""
+    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -25,9 +25,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
 
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train,labels_train)
+t1 = time()
+print "training time:", round(t1-t0, 3), "s"
+t2 = time()
+pre = clf.predict(features_test)
+#print pre
+#print features_test
+t3 = time()
+print "prediction time:", round(t3-t2, 3), "s"
+
+score = clf.score(features_test,labels_test)
+
+print score
 
 #########################################################
-
-
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,7 +24,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
 
+clf = SVC(kernel='rbf',C=10000.)
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+t0 = time()
+clf.fit(features_train,labels_train)
+t1 = time()
+print "training time:", round(t1-t0, 3), "s"
+print "test length: "+str(len(features_test))
+t2 = time()
+pre = clf.predict(features_test)
+t3 = time()
+print "chris(1): "+str((pre.tolist()).count(1))
+print "prediction time:", round(t3-t2, 3), "s"
+
+score = clf.score(features_test,labels_test)
+
+print score
 #########################################################
-
-
